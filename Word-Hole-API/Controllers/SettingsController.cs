fe283ae396b3a6464587c4a106cd3d0e6e8c317f
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2020-01-01 00:41:28 -0800

    Ability to update passwords

---
+++ Word-Hole-API/Controllers/SettingsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Word_Hole_API.Models.DB;
+using Word_Hole_API.Models.Settings;
+
+namespace Word_Hole_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        private readonly WordHoleDBContext _context;
+
+        public SettingsController(WordHoleDBContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpPatch("password")]
+        public IActionResult PatchPassword(UpdateProfilePatch parameters)
+        {
+            if (parameters.New.Count() < 8)
+                return BadRequest(new { error = "Password must be at least 8 characters long" });
+
+            if (parameters.New != parameters.Confirm)
+                return BadRequest(new { error = "Passwords do not match" });
+
+            var userID = int.Parse(HttpContext.User.Claims.Single(c => c.Type == "UserID").Value);
+
+            var user = (from users in _context.Users
+                        where users.Id == userID
+                        select users).Single();
+
+            if (!BCrypt.Net.BCrypt.Verify(parameters.Current, user.Hash))
+                return BadRequest(new { error = "Current password incorrect" });
+
+            var newHash = BCrypt.Net.BCrypt.HashPassword(parameters.New);
+
+            user.Hash = newHash;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Word-Hole-API/Models/Settings/UpdateProfilePatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Word_Hole_API.Models.Settings
+{
+    public class UpdateProfilePatch
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Current { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string New { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Confirm { get; set; }
+    }
+}
