99ac00734ff7e5ab40574524a7d2ac0846e20812
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2020-01-03 22:56:09 -0800

    Try to fix timezone bug caused by heroku servers not running Windows

--- Word-Hole-API/Common.cs
+++ Word-Hole-API/Common.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Word_Hole_API
@@ -10,7 +11,22 @@ namespace Word_Hole_API
         public static string GetPSTTimeString(DateTime time)
         {
             var format = "dddd, MMM dd, yyyy hh:mm tt";
-            var adjustedTime = TimeZoneInfo.ConvertTime(time, TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time"));
+            TimeZoneInfo timeZone;
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                timeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                timeZone = TimeZoneInfo.FindSystemTimeZoneById("America/Los_Angeles");
+            }
+            else
+            {
+                return time.ToString(format);
+            }
+
+            var adjustedTime = TimeZoneInfo.ConvertTime(time, timeZone);
 
             return adjustedTime.ToString(format);
         }
